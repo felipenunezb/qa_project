91f55b916ab8d26d587be3cacbcd623919774841
felipenunezb <56325305+felipenunezb@users.noreply.github.com>
2020-08-15 19:36:39 +1000

    testing something

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -738,7 +738,7 @@ class Trainer:
 
         model.train()
         inputs = self._prepare_inputs(inputs, model)
-        print(inputs[0])
+        print(inputs)
 
         if self.args.fp16 and _use_native_amp:
             with autocast():
