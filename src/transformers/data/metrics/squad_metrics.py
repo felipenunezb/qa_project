76e9ebb1d154cabdbf6824bdc41f7b828b0a2449
felipenunezb <56325305+felipenunezb@users.noreply.github.com>
2020-09-07 09:13:28 +1000

    testing

--- src/transformers/data/metrics/squad_metrics.py
+++ src/transformers/data/metrics/squad_metrics.py
@@ -691,6 +691,7 @@ def compute_predictions_logits_vqa(
             )
         prelim_predictions = sorted(prelim_predictions, key=lambda x: (x.start_logit + x.end_logit), reverse=True)
         prelim_choices = sorted(prelim_choices, key=lambda x: (x.choice_logit), reverse=True)
+        print(f"prelim choices: {len(prelim_choices)}")
 
         #Choice calculations
         _NbestPrediction_choice = collections.namedtuple(  # pylint: disable=invalid-name
@@ -700,7 +701,7 @@ def compute_predictions_logits_vqa(
         nbest_choice = []
         for pred in prelim_choices:
             nbest_choice.append(_NbestPrediction_choice(text=ix_to_ans_dict.get(pred.choice_index), choice_logit=pred.choice_logit))
-
+        print(f"nbest_choice: {len(nbest_choice)}")
         total_choice_scores = []
         best_choice_entry = None
         for entry in nbest_choice:
@@ -708,6 +709,7 @@ def compute_predictions_logits_vqa(
             if not best_choice_entry:
                 if entry.text:
                     best_choice_entry = entry
+        print(f"best_choice_entry: {best_choice_entry}")
 
         choice_probs = _compute_softmax(total_choice_scores)
 
