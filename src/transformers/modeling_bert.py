e19a13b50f08e787853d7a7c032a8b17ee877112
felipenunezb <56325305+felipenunezb@users.noreply.github.com>
2020-09-12 10:14:40 +1000

    testing

--- src/transformers/modeling_bert.py
+++ src/transformers/modeling_bert.py
@@ -1667,7 +1667,7 @@ class BertForQuestionAnsweringVQAPool_MultiVote(BertPreTrainedModel):
         sequence_output = outputs[0]
 
         if scene_dataset:
-            print(f"scene dataset: {sceneData['1']['objects']['1058498']}")
+            print(f"scene dataset: {scene_dataset['1']['objects']['1058498']}")
 
         logits = self.qa_outputs(sequence_output)
         start_logits, end_logits = logits.split(1, dim=-1)
