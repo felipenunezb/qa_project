7662e6b8064d22ac7c3163d05da0108841bc8304
felipenunezb <56325305+felipenunezb@users.noreply.github.com>
2020-09-15 01:14:51 +1000

    cat of tuple

--- src/transformers/modeling_bert.py
+++ src/transformers/modeling_bert.py
@@ -2204,9 +2204,9 @@ class BertForQuestionAnsweringVQAPool_MultiVote(BertPreTrainedModel):
         end_logits = end_logits.squeeze(-1)
 
         #mean of second to last layer of hidden states
-        voter_1 = torch.mean(torch.cat(outputs[2][-2],scenedata,dim=1), dim=1)
+        voter_1 = torch.mean(torch.cat((outputs[2][-2],scenedata),dim=1), dim=1)
         #mean of last layer of hidden states
-        voter_2 = torch.mean(torch.cat(outputs[2][-1],scenedata,dim=1), dim=1)
+        voter_2 = torch.mean(torch.cat((outputs[2][-1],scenedata),dim=1), dim=1)
         #mean of both voters
         voter_3 = torch.mean(torch.stack([voter_1, voter_2]), dim=0)
         #max of both voters
