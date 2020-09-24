72fbf3e6f04babc2e2cfebcf4439977ee982f2fc
felipenunezb <56325305+felipenunezb@users.noreply.github.com>
2020-09-24 22:20:22 +1000

    prints

--- src/transformers/modeling_bert.py
+++ src/transformers/modeling_bert.py
@@ -224,6 +224,47 @@ class BertEmbeddings(nn.Module):
         embeddings = self.dropout(embeddings)
         return embeddings
 
+class SGBertEmbeddings(nn.Module):
+    """Construct the embeddings from word, position and token_type embeddings.
+    """
+
+    def __init__(self, config):
+        super().__init__()
+        self.word_embeddings = nn.Embedding(config.vocab_size, config.hidden_size, padding_idx=config.pad_token_id)
+        self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size)
+        self.token_type_embeddings = nn.Embedding(config.type_vocab_size, config.hidden_size)
+
+        # self.LayerNorm is not snake-cased to stick with TensorFlow model variable name and be able to load
+        # any TensorFlow checkpoint file
+        self.LayerNorm = BertLayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+
+        # position_ids (1, len position emb) is contiguous in memory and exported when serialized
+        self.register_buffer("position_ids", torch.arange(config.max_position_embeddings).expand((1, -1)))
+
+    def forward(self, input_ids=None, token_type_ids=None, position_ids=None, inputs_embeds=None):
+        if input_ids is not None:
+            input_shape = input_ids.size()
+        else:
+            input_shape = inputs_embeds.size()[:-1]
+
+        seq_length = input_shape[1]
+
+        if position_ids is None:
+            position_ids = self.position_ids[:, :seq_length]
+
+        if token_type_ids is None:
+            token_type_ids = torch.zeros(input_shape, dtype=torch.long, device=self.position_ids.device)
+
+        if inputs_embeds is None:
+            inputs_embeds = self.word_embeddings(input_ids)
+        position_embeddings = self.position_embeddings(position_ids)
+        token_type_embeddings = self.token_type_embeddings(token_type_ids)
+
+        embeddings = inputs_embeds + position_embeddings + token_type_embeddings
+        embeddings = self.LayerNorm(embeddings)
+        embeddings = self.dropout(embeddings)
+        return embeddings
 
 class BertSelfAttention(nn.Module):
     def __init__(self, config):
@@ -1158,7 +1199,7 @@ class BertModelE(BertPreTrainedModel):
         self.encoder = BertEncoder(config)
         self.pooler = BertPooler(config)
         self.scene_emb = LoadSceneGraph_dict(config)
-        self.linear2 = nn.Linear(150, 128)
+        self.linear_scene = nn.Linear(150, 128)
 
         self.init_weights()
 
@@ -1251,13 +1292,16 @@ class BertModelE(BertPreTrainedModel):
         embedding_output = self.embeddings(
             input_ids=input_ids, position_ids=position_ids, token_type_ids=token_type_ids, inputs_embeds=inputs_embeds
         )
+        print(embedding_output[0, :, :])
 
         #print(f"embedding_output: {embedding_output.shape}")
         scenedata = self.scene_emb(titles, scene_dataset, embedding, scene_dict)
-        #print(f"prior scenedata: {scenedata.shape}")
+        print(f"prior scenedata: {scenedata.shape}")
         #Only keep the first 128 objects, to avoid going beyond the 512 tokens
-        scenedata = self.linear2(scenedata) #scenedata[:, :128, :] 
+        scenedata = self.linear_scene(scenedata) #scenedata[:, :128, :] 
         #print(f"after scenedata: {scenedata.shape}")
+
+
 
         #Concat regular embedding plus 128 objects scene graph embedding
         embedding_output = torch.cat((embedding_output[:,:-1,:], scenedata, embedding_output[:,-1,:]), dim=1)
