0dfb437f68195e1b0f5ca88b8099ce018bf70aa4
felipenunezb <56325305+felipenunezb@users.noreply.github.com>
2020-09-25 12:54:51 +1000

    remove commented lines

--- src/transformers/modeling_bert.py
+++ src/transformers/modeling_bert.py
@@ -1256,14 +1256,14 @@ class BertModelE(BertPreTrainedModel):
         )
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
-        #if input_ids is not None and inputs_embeds is not None:
-        #    raise ValueError("You cannot specify both input_ids and inputs_embeds at the same time")
-        #elif input_ids is not None:
-        #    input_shape = input_ids.size()
-        #elif inputs_embeds is not None:
-        #    input_shape = inputs_embeds.size()[:-1]
-        #else:
-        #    raise ValueError("You have to specify either input_ids or inputs_embeds")
+        if input_ids is not None and inputs_embeds is not None:
+            raise ValueError("You cannot specify both input_ids and inputs_embeds at the same time")
+        elif input_ids is not None:
+            input_shape = input_ids.size()
+        elif inputs_embeds is not None:
+            input_shape = inputs_embeds.size()[:-1]
+        else:
+            raise ValueError("You have to specify either input_ids or inputs_embeds")
 
         device = input_ids.device if input_ids is not None else inputs_embeds.device
 
