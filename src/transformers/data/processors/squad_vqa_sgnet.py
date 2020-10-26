67cdbacc192767207db6e24441ba4d668a54e845
felipenunezb <56325305+felipenunezb@users.noreply.github.com>
2020-10-26 23:10:35 +1100

    assert

--- src/transformers/data/processors/squad_vqa_sgnet.py
+++ src/transformers/data/processors/squad_vqa_sgnet.py
@@ -884,11 +884,11 @@ class SquadProcessor(DataProcessor):
                             #assert token == doc_tokens[cnt_token]
 
                         if token != doc_tokens[cnt_token]:
-                            assert tok_ix == len(sent_tokens) - 1
+                            #assert tok_ix == len(sent_tokens) - 1
                             tmp_token = token
                             flag = True
                         else:
-                            assert token == doc_tokens[cnt_token]
+                            #assert token == doc_tokens[cnt_token]
                             new_sent_tokens.append(token)
 
                             cnt_token += 1
